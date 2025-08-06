03dbfb3a5aac27280d696c221c257d140bebe3ca
mabdullahsoyturk <msoyturk@gams.com>
2025-08-06 11:20:43 +0200

    add missing traffic.gdx

--- test_models.py
+++ test_models.py
@@ -8,9 +8,13 @@ def test_all_models():
     for model in all_py_files:
         with open(model) as file:
             content = file.read()
-            if "LICENSETYPE: Requires license" in content or "LICENSETYPE: Community" in content or "hansmge" in model:
+            if (
+                "LICENSETYPE: Requires license" in content
+                or "LICENSETYPE: Community" in content
+                or "hansmge" in model
+            ):
                 continue
-            
+
         print(f"Running {model}...")
-        return_code = subprocess.call(["python", model])
-        assert return_code == 0
+        process = subprocess.run(["python", model], capture_output=True, text=True)
+        assert process.returncode == 0, process.stderr
